b3384d8b4d9c0fab58214f30acd47ccbed549cee
jsmar <jsmarino1993@outlook.com>
2023-08-28 21:20:47 -0300

    28_08_23

---
+++ TopicosAvancadoseEmSI_II/28_08_23/ExemploAPI/28_08_23_Atividade/ProjWebApi28082023/Data/ProjWebApi28082023Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjWebApi28082023.Models;
+
+namespace ProjWebApi28082023.Data
+{
+    public class ProjWebApi28082023Context : DbContext
+    {
+        public ProjWebApi28082023Context (DbContextOptions<ProjWebApi28082023Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjWebApi28082023.Models.Person> Person { get; set; } = default!;
+    }
+}
---
+++ TopicosAvancadoseEmSI_II/28_08_23/ExemploAPI/28_08_23_Atividade/ProjWebApi28082023/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjWebApi28082023.Models
+{
+	public class Person
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string City { get; set; }
+	}
+}
