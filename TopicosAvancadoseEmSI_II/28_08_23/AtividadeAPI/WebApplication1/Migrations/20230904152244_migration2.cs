7c6740563fa98e42b1b36b4dd77d534f1f328e8d
jsmar <jsmarino1993@outlook.com>
2023-09-04 14:35:12 -0300

    04_09_23

---
+++ TopicosAvancadoseEmSI_II/28_08_23/AtividadeAPI/WebApplication1/Migrations/20230904152244_migration2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    public partial class migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "c",
+                table: "Equacao",
+                newName: "C");
+
+            migrationBuilder.RenameColumn(
+                name: "b",
+                table: "Equacao",
+                newName: "B");
+
+            migrationBuilder.RenameColumn(
+                name: "a",
+                table: "Equacao",
+                newName: "A");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "C",
+                table: "Equacao",
+                newName: "c");
+
+            migrationBuilder.RenameColumn(
+                name: "B",
+                table: "Equacao",
+                newName: "b");
+
+            migrationBuilder.RenameColumn(
+                name: "A",
+                table: "Equacao",
+                newName: "a");
+        }
+    }
+}
--- TopicosAvancadoseEmSI_II/28_08_23/AtividadeAPI/WebApplication1/Models/Equacao.cs
+++ TopicosAvancadoseEmSI_II/28_08_23/AtividadeAPI/WebApplication1/Models/Equacao.cs
@@ -1,22 +1,53 @@
-﻿namespace WebApplication1.Models
+﻿using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+namespace WebApplication1.Models
 {
 	public class Equacao
 	{
 		public int Id { get; set; }
-		public double a { get; set; }
-		public double b { get; set; }
-		public double c { get; set; }
-		public double delta
+		public double A { get; set; }
+		public double B { get; set; }
+		public double C { get; set; }
+		[JsonIgnore] // Isso impede que a propriedade Delta seja serializada
+		public double Delta
 		{
-			get { return (b * b) - (4 * a * c); }
+			get
+			{
+				return (B * B) - (4 * A * C);
+			}
+
 		}
-		public double Raiz1()
+
+		public double X1 { get => Raiz1(X1); }
+		public double Raiz1(double x1)
 		{
-			get { return (-b + Math.Sqrt(delta)) / (2 * a); }
+			if (Delta >= 0)
+			{
+				x1 = (-B + Math.Sqrt(Delta)) / (2 * A);
+				return x1;
+			}
+			else
+			{
+				// Trate o caso de raízes complexas ou delta negativo aqui, se necessário.
+				// Pode retornar NaN (Not-a-Number) ou outra coisa apropriada.
+				return x1;
+			}
 		}
-		public double Raiz2()
+
+		public double X2 { get => Raiz2(X2);}
+		public double Raiz2(double x2)
 		{
-			return (-b - Math.Sqrt(delta)) / (2 * a);
+			if (Delta >= 0)
+			{
+				x2 = (-B - Math.Sqrt(Delta)) / (2 * A);
+				return x2;
+			}
+			else
+			{
+				// Trate o caso de raízes complexas ou delta negativo aqui, se necessário.
+				// Pode retornar NaN (Not-a-Number) ou outra coisa apropriada.
+				return x2;
+			}
 		}
 	}
 }
