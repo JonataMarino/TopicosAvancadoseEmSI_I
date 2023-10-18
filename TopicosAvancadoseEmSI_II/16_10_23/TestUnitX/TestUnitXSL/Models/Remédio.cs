a0a06302674dbef8973f19b2bd5e7c581e4220b2
jsmar <jsmarino1993@outlook.com>
2023-10-18 15:05:18 -0300

    18_10_23

---
+++ TopicosAvancadoseEmSI_II/16_10_23/TestUnitX/TestUnitXSL/Models/Remédio.cs
@@ -0,0 +1,14 @@
+﻿using System.Security;
+
+namespace TestUnitXSL.Models
+{
+	public class Remédio
+	{
+		public int Id { get; set; }
+		public string Descricao { get; set; }
+		public decimal valor {  get; set; }
+		//var today = DateOnly.FromDateTime(DateTime.Now);
+		public string dataCadastro {  get; set; }
+		public string dataValidade { get; set; }
+	}
+}
