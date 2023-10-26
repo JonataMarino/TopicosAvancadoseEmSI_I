bd32b2ec12564dc3c75bb7a7a8f00d254bbbdd99
jsmar <jsmarino1993@outlook.com>
2023-10-26 16:00:55 -0300

    26_10_23

--- TopicosAvancadoseEmSI_II/25_09_23/ControleCompraEVendas.test/UnitTest1.cs
+++ TopicosAvancadoseEmSI_II/25_09_23/ControleCompraEVendas.test/UnitTest1.cs
@@ -1,11 +1,44 @@
+using ControleCompraEVendas.Controllers;
+using ControleCompraEVendas.Data;
+using ControleCompraEVendas.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 namespace ControleCompraEVendas.test
 {
 	public class UnitTest1
 	{
-		[Fact]
-		public void Test1()
-		{
+		private DbContextOptions<ControleCompraEVendasContext> options;
 
+		private void InitializeDataBase()
+		{
+			//Criar DataBase Temporário
+			//Instalar entityFramwork.InMemory pelo nuget
+			options = new DbContextOptionsBuilder<ControleCompraEVendasContext>()
+				.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+			//inserir dados no DB usando context
+			using (var context = new ControleCompraEVendasContext(options))
+			{
+				context.Clientes.Add(new Clientes {Id = 1, Name = "Jonatã", Cpf = "22244455543", telefone = "65778765554", endereco = "rua com asfalto S/N" });
+				context.SaveChanges();
+			}
+		}
+
+		[Fact]
+		public void TodosOsClientes()
+		{
+			InitializeDataBase();
+
+			//use a clean instance of context to run the test
+			using (var context = new ControleCompraEVendasContext(options))
+			{
+				ClientesController clientesController = new ClientesController(context);
+				IEnumerable<Clientes> clientes = clientesController.GetClientes().Result.Value;
+				Assert.Equal(1, clientes.Count());
+			}
 		}
 	}
 }
+
+	
