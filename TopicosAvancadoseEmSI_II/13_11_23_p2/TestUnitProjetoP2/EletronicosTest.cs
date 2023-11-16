c898f631579c0b7c57afa6870a8e2bef79906499
jsmar <jsmarino1993@outlook.com>
2023-11-16 11:34:39 -0300

    16_11_23]

---
+++ TopicosAvancadoseEmSI_II/13_11_23_p2/TestUnitProjetoP2/EletronicosTest.cs
@@ -0,0 +1,101 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoP2.Controllers;
+using ProjetoP2.Data;
+using ProjetoP2.Models;
+
+namespace TestUnitProjetoP2
+{
+    public class EletronicosTest
+    {
+        private DbContextOptions<ProjetoP2Context> options;
+
+        private void InitializeDatabase()
+        {
+            options = new DbContextOptionsBuilder<ProjetoP2Context>().
+                UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).
+                Options;
+
+            using (var context = new ProjetoP2Context(options))
+            {
+                context.Eletronicos.Add(new Eletronicos { Id = 1, Nome = "Monitor portátil", Marca = "samsung", Modelo = "TLW-11/22-24.5'MHRZ", Ano = "2022/23", Descricao = "Monitor gamer 24' FHD IPS"  });
+                context.Eletronicos.Add(new Eletronicos { Id = 2, Nome = "Monitor portátil", Marca = "samsung", Modelo = "TLW-11/22-24.5'MHRZ", Ano = "2022/23", Descricao = "Monitor gamer 24' FHD IPS"  });
+                context.Eletronicos.Add(new Eletronicos { Id = 3, Nome = "Monitor portátil", Marca = "samsung", Modelo = "TLW-11/22-24.5'MHRZ", Ano = "2022/23", Descricao = "Monitor gamer 24' FHD IPS"  });
+                context.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public void GetAllEletronicos() 
+        {
+            InitializeDatabase();
+
+            using (var context = new ProjetoP2Context(options))
+            {
+                EletronicosController eletronicosController = new EletronicosController(context);
+                IEnumerable<Eletronicos> eletronicos = eletronicosController.GetEletronicos().Result.Value;
+                Assert.Equal(3, eletronicos.Count());
+            }
+        }
+
+        [Fact]
+        public void GetEletronicoById()
+        {
+            InitializeDatabase();
+            using (var context = new ProjetoP2Context(options))
+            {
+                int id = 3;
+                EletronicosController eletronicosController = new EletronicosController(context);
+                Eletronicos eletronicos = eletronicosController.GetEletronicos(id).Result.Value;
+                Assert.Equal(3, eletronicos.Id);
+            }
+        }
+
+        [Fact]
+        public async void CreateEletronico()
+        {
+            InitializeDatabase();
+            Eletronicos eletronicos = new Eletronicos()
+            {
+                Id = 4,
+                Nome = "Controle Xbox",
+                Marca = "Microsoft",
+                Modelo = "XBX11/22-2023 AIO",
+                Ano = "2022/23",
+                Descricao = "Controle xBox compatível com todas as plataformas"
+            };
+
+            using  (var context = new ProjetoP2Context(options))
+            {
+                EletronicosController eletronicosController = new EletronicosController(context);
+                await eletronicosController.PostEletronicos(eletronicos);
+                Eletronicos eletronicosReturn = eletronicosController.GetEletronicos(4).Result.Value;
+                Assert.Equal("Controle Xbox", eletronicosReturn.Nome);
+            }
+        }
+
+        [Fact]
+        public async void UpdateEletronico()
+        {
+            InitializeDatabase();
+
+            Eletronicos eletronicos = new Eletronicos()
+            {
+                Id = 1,
+                Nome = "PlayStation V",
+                Marca = "Sony",
+                Modelo = "Soft",
+                Ano = "2023",
+                Descricao = "Video Game SonyConnect"
+
+            };
+
+            using (var context = new ProjetoP2Context(options))
+            {
+                EletronicosController eletronicosController = new EletronicosController(context);
+                await eletronicosController.PutEletronicos(1, eletronicos);
+                Eletronicos eletronicosReturn = eletronicosController.GetEletronicos(1).Result.Value;
+                Assert.Equal("PlayStation V", eletronicosReturn.Nome);
+            }
+        }
+    }
+}
---
+++ TopicosAvancadoseEmSI_II/13_11_23_p2/TestUnitProjetoP2/VeiculosTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoP2.Data;
+using ProjetoP2.Models;
+
+namespace TestUnitProjetoP2
+{
+    public class VeiculosTest
+    {
+        private DbContextOptions<ProjetoP2Context> options;
+
+        private void InitializeDatabase()
+        {
+            options = new DbContextOptionsBuilder<ProjetoP2Context>().
+            UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            
+            using (var context = new ProjetoP2Context(options))
+            {
+                context.Eletronicos.Add(new Eletronicos { Id = 1, Nome = "Monitor portátil", Marca = "samsung", Modelo = "TLW-11/22-24.5'MHRZ", Ano = "2022/23", Descricao = "Monitor gamer 24' FHD IPS" });
+                context.Eletronicos.Add(new Eletronicos { Id = 2, Nome = "Monitor portátil", Marca = "samsung", Modelo = "TLW-11/22-24.5'MHRZ", Ano = "2022/23", Descricao = "Monitor gamer 24' FHD IPS" });
+                context.Eletronicos.Add(new Eletronicos { Id = 3, Nome = "Monitor portátil", Marca = "samsung", Modelo = "TLW-11/22-24.5'MHRZ", Ano = "2022/23", Descricao = "Monitor gamer 24' FHD IPS" });
+                context.SaveChanges();
+            }
+        }
+        [Fact]
+        public void GetAllVeiculos()
+        {
+           
+        }
+    }    
+}
