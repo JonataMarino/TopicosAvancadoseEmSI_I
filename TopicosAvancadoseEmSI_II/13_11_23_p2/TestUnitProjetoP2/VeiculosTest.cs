fdbcc6e764ccdaf058854b60ea988af990219a1f
jsmar <jsmarino1993@outlook.com>
2023-11-21 10:46:18 -0300

    21_11_23

--- TopicosAvancadoseEmSI_II/13_11_23_p2/TestUnitProjetoP2/VeiculosTest.cs
+++ TopicosAvancadoseEmSI_II/13_11_23_p2/TestUnitProjetoP2/VeiculosTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ProjetoP2.Controllers;
 using ProjetoP2.Data;
 using ProjetoP2.Models;
 
@@ -17,19 +18,38 @@ namespace TestUnitProjetoP2
         {
             options = new DbContextOptionsBuilder<ProjetoP2Context>().
             UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
-            
+
             using (var context = new ProjetoP2Context(options))
             {
-                context.Eletronicos.Add(new Eletronicos { Id = 1, Nome = "Monitor portátil", Marca = "samsung", Modelo = "TLW-11/22-24.5'MHRZ", Ano = "2022/23", Descricao = "Monitor gamer 24' FHD IPS" });
-                context.Eletronicos.Add(new Eletronicos { Id = 2, Nome = "Monitor portátil", Marca = "samsung", Modelo = "TLW-11/22-24.5'MHRZ", Ano = "2022/23", Descricao = "Monitor gamer 24' FHD IPS" });
-                context.Eletronicos.Add(new Eletronicos { Id = 3, Nome = "Monitor portátil", Marca = "samsung", Modelo = "TLW-11/22-24.5'MHRZ", Ano = "2022/23", Descricao = "Monitor gamer 24' FHD IPS" });
+                context.Veiculos.Add(new Veiculos { Id = 1, Fabricante = "FIAT", Modelo = "Toro", Cor = "Preto", Ano = "2022/23" });
+                context.Veiculos.Add(new Veiculos { Id = 2, Fabricante = "GM MORTORS", Modelo = "Onix Turbo", Cor = "Prata", Ano = "2022/23" });
+                context.Veiculos.Add(new Veiculos { Id = 3, Fabricante = "Honda", Modelo = "HRV", Cor = "Cinza", Ano = "2022/23" });
                 context.SaveChanges();
             }
         }
         [Fact]
         public void GetAllVeiculos()
         {
-           
+            InitializeDatabase();
+
+            using (var context = new ProjetoP2Context(options))
+            {
+                VeiculosController veiculosController = new VeiculosController(context);
+                IEnumerable<Veiculos> veiculos = veiculosController.GetVeiculos().Result.Value;
+                Assert.Equal(3, veiculos.Count());
+            }
         }
-    }    
+        [Fact]
+        public async Task GetVeiculoById()
+        {
+            InitializeDatabase();
+            using (var context = new ProjetoP2Context(options))
+            {
+                int id = 3;
+                VeiculosController veiculosController = new VeiculosController(context);
+                Veiculos veiculos = veiculosController.GetVeiculos(id).Result.Value;
+                Assert.Equal(3, veiculos.Id);
+            }
+        }
+    }
 }
